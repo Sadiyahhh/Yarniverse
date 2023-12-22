f136b82a28d2a44cc426e350dc591b2a0af6eab4
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2023-12-22 20:14:20 +0000

    progress on navbar + footer!

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,6 +7,10 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
+        <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
 
         <title>Yarniverse</title>
         <link rel="icon" type="image/x-icon" href="/site-images/icon-nobg.png">
@@ -25,10 +29,21 @@
     </div> -->
 
     <div class="navigation">
-        <div class="nav-logo" id="nav-logo"><img src="/site-images/y-nobg.png" alt="" href="/"></img>
+        <div class="nav-logo" id="nav-logo">
+            <img src="/site-images/y-nobg.png" alt="" href="/"></img>
         </div>
         <div class="nav-search"> 
-
+            <!-- <div class="overlap-group"> -->
+                <div class="search-1">
+                    <div class="search-icon">
+                        <button type="submit" id="search-icon"><iconify-icon icon="heroicons:magnifying-glass"></iconify-icon></button>
+                    </div>
+                    <form action="/welcome" class="search-bar">
+                        <input type="Search" name="search" id="search" placeholder="Search our patterns">
+                    </form>
+                </div>
+                
+            <!-- </div> -->
         </div>
         <div class="nav-items"> 
             <span class="material-symbols-outlined" id="account-icon" href="/myaccount">person</span>
@@ -37,3 +52,76 @@
         </div>
     </div>
 
+
+    <div class="footer">
+        <div class="footer-content">
+            <div class="footer-logo">
+                <a href="/" class="logo"><img src="/site-images/lightvert-nobg.png" alt="" /></a>
+            </div>
+
+            <div class="footer-links" id="foot-content-box">
+                    <a href="/faq">FAQs</a><br>
+                    <a href="/contact">Contact Us</a><br>
+                    <a href="/tc">T&Cs</a><br>
+                    <a href="/privacy">Privacy Policy</a><br>
+            </div>
+        </div>
+        <div class="newsletter" id="foot-content-box" style="width:20%; margin-left: 75%; margin-top:-10%;">
+                <form action="/" method="post" >
+                @csrf
+                    <div class="news-text">
+                        <h3>Get 20% off your next pattern purchase</h3>
+                        <p>Be the first to hear about our latest deals and more!</p>
+                    </div>
+
+                    <br>
+
+                    <div class="news-email">
+                        <input type="text" id="news-email" placeholder="Email address" name="email" required><br>
+                    </div>
+
+                    <br>
+
+                    <div class="news-submit">
+                        <input type="submit" id="news-submit" value="Submit">
+                    </div>
+                </form>
+        </div>
+        <br>
+        <br>
+        <hr>
+        
+        <div class="footer-bottom">
+          <div class="d-flex justify-content-between my-4">
+            <div class="col-md-4 d-flex align-items-center" style="width: 31%;">
+              <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+              </a>
+              <br>
+              <span class= "mb-md-0 text-muted" style="color: #D0B7B4;">&copy; Copyright Yarniverse LTD, 2023</span>
+            </div>
+
+            <div class="social">
+                <div class="facebook">
+                    <a href="https://www.facebook.com/" target="_blank">
+                        <i class='bx bxl-facebook  bx-sm'></i>
+                    </a>
+                </div>
+                <div class="instagram">
+                    <a href="https://www.instagram.com/" target="_blank">
+                        <i class='bx bxl-instagram bx-sm'></i>
+                    </a>
+                </div>
+                <div class="tiktok">
+                    <a href="https://www.tiktok.com/" target="_blank">
+                        <i class='bx bxl-tiktok bx-sm'></i>
+                    </a>
+                </div>
+                  <div class="twitter">
+                    <a href="https://www.twitter.com/" target="_blank">
+                        <i class='bx bxl-twitter bx-sm'></i>
+                    </a>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>  
