077fbe21c21fa2f6f8fd823470c6d4a4906b6d67
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2024-03-22 16:50:06 +0000

    login/register function

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,3 +6,4 @@
 
 <a class="active" href="/welcome" style = "color: red;">Welcome</a><br>
 <a class="active" href="/contact" style = "color: red;">Contact</a><br>
+<a class="li" href="{{ route('login') }}">Login</a>
