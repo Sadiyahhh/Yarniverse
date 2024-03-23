0233022e49a4180ede83b6311aedb8eb6a379040
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2024-03-23 16:10:11 +0000

    finished login function + minor edits

--- resources/views/contactus.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.layout')
-
-@section('css')
-<link rel="stylesheet" href="/css/contact.css">
-@endsection
-
-<p>Contact Us<p>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contactus', function () {
+    return view('contactus');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
