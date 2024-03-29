2d02ae241b4daa34698aec688474cec752552ead
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2024-03-29 13:29:20 +0000

    product controller, model

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+}
